844c895148366effa412fde2be9a0775af5b2e2d
Norah Lu <101174526+NalianLu@users.noreply.github.com>
2023-06-06 21:34:16 -0500

    add ex14

---
+++ python/ex14.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  6 08:39:18 2023
+
+@author: Group 2
+"""
+# package
+import math
+
+# input
+a = int(input('Enter the value for a:'))
+b = int(input('Enter the value for b:'))
+c = int(input('Enter the value for c:'))
+
+# calculation & output
+if b**2-4*a*c > 0:
+    x1 = (-b+math.sqrt(b**2-4*a*c))/(2*a)
+    x2 = (-b-math.sqrt(b**2-4*a*c))/(2*a)
+    print('Solution: {0:.2f}, {1:.2f}'.format(x1, x2))
+elif b**2-4*a*c == 0:
+    x = (-b)/(2*a)
+    print('Solution: {0:.2f}'.format(x))
+else:
+    print('No solutions.')
