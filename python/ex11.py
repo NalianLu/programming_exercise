f070fb36da9a3a4fd9867a6eb7dd91603eb42a08
Norah Lu <101174526+NalianLu@users.noreply.github.com>
2023-06-06 21:34:03 -0500

    add ex11

---
+++ python/ex11.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  6 08:57:02 2023
+
+@author: Group 2
+"""
+
+n = int(input('Please enter the maximum denominator: '))
+
+sum = 0  # initialize
+
+while n > 0:
+    sum = sum+(1/n)
+    n -= 1
+
+print('The sum is {0}.'.format(sum))
